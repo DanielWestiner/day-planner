aef5b1c43f1dacf713c70150f32d62426dede549
DanielWestiner <daniel.westiner@gmail.com>
2021-12-17 01:37:44 -0500

    changed how local storage works, cleaned up js

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,13 @@ var today = moment();
 var dayWeek = today.format("[Today is] dddd, MMMM Do");
 $("#currentDay").text(dayWeek);
 
-// Setting up each hour variable as array. 24hr time to make it easier to calculate
-
-let hoursOfDay = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-
 // Creating colorized note blocks - use jQuery .each to iterate and execute function over textarea
 
 let currentHour = parseInt(moment().format("H"));
+
+// Compares current time hour to name in text area
+
+// Set up the name in text area in 24hr time to make it easier to calculate
 
 $("textarea").each(function () {
   let color = parseInt($(this).attr("name"));
@@ -25,52 +25,33 @@ $("textarea").each(function () {
   }
 });
 
+// jQuery Event Listener on click and added items to local storage when save button is clicked
+let saveButton = $(".btn");
+saveButton.on("click", function save() {
+  let fieldValue1 = document.getElementById("9AM").value;
+  localStorage.setItem("9notes", fieldValue1);
 
+  let fieldValue2 = document.getElementById("10AM").value;
+  localStorage.setItem("10notes", fieldValue2);
 
-//  Setting up variable to easily store into local storage and append
-let nine = $("#9AM");
-let ten = $("#10AM");
-let eleven = $("#11AM");
-let twelve = $("#12PM");
-let one = $("#1PM");
-let two = $("#2PM");
-let three = $("#3PM");
-let four = $("#4PM");
-let five = $("#5PM");
+  let fieldValue3 = document.getElementById("11AM").value;
+  localStorage.setItem("11notes", fieldValue3);
 
+  let fieldValue4 = document.getElementById("12PM").value;
+  localStorage.setItem("12notes", fieldValue4);
 
-let row1 = $(".row1");
-let row2 = $(".row2");
-let row3 = $(".row3");
-let row4 = $(".row4");
-let row5 = $(".row5");
-let row6 = $(".row6");
-let row7 = $(".row7");
-let row8 = $(".row8");
-let row9 = $(".row9");
+  let fieldValue5 = document.getElementById("1PM").value;
+  localStorage.setItem("1notes", fieldValue5);
 
+  let fieldValue6 = document.getElementById("2PM").value;
+  localStorage.setItem("2notes", fieldValue6);
 
-// jQuery Event Listener on click
-var saveButton = $(".btn");
-saveButton.on("click", function () {
-  localStorage.setItem = ("nine", "nine.val()");
-  localStorage.setItem = ("ten", "ten.val()");
-  localStorage.setItem = ("eleven", "eleven.val()");
-  localStorage.setItem = ("twelve", "twelve.val()");
-  localStorage.setItem = ("one", "one.val()");
-  localStorage.setItem = ("two", "two.val()");
-  localStorage.setItem = ("three", "three.val()");
-  localStorage.setItem = ("four", "four.val()");
-  localStorage.setItem = ("five", "five.val()");
+  let fieldValue7 = document.getElementById("3PM").value;
+  localStorage.setItem("3notes", fieldValue7);
 
+  var fieldValue8 = document.getElementById("4PM").value;
+  localStorage.setItem("4notes", fieldValue8);
 
-//   if (localStorage.getItem("nine") !== null) {
-//     let savedNote = localStorage.getItem("nine");
-//     nineAM.text(savedNote);
-//     row1.append(nineAM);
-//   }
+  var fieldValue9 = document.getElementById("5PM").value;
+  localStorage.setItem("5notes", fieldValue9);
 });
-
-$(".row1").append(localStorage.getItem("nine"));
-
-
